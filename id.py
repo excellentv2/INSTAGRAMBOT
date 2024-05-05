09645e133be8f890191d569de7cd64908d41351c
excellent <152197688+excellentv2@users.noreply.github.com>
2024-05-05 10:54:29 -0700

    Update id.py

--- id.py
+++ id.py
@@ -10,9 +10,10 @@ password = sys.argv[2]
 target_username = sys.argv[3]
 
 bot = Bot()
-bot.login(username=username, password=password)
+bot.login(username=username, password=password, proxy=None)
 
 user_id = bot.get_user_id_from_username(target_username)
 print("ID пользователя:", user_id)
 
 bot.logout()
+
